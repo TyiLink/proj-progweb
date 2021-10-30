4726c52d9efacdd9bd1a29ae617fe92412b192a4
TyiLink <andre.ncor@gmail.com>
2021-10-30 15:24:37 -0300

    v0.8

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
-function toggle_mobile_menu() {
-    document.getElementById("mobile-menu").classList.toggle('open');
-}
+const bt_mm = document.querySelector('.menu-btn');
+const mm = document.querySelector('.mobile-menu')
+let mm_state = false;
+bt_mm.addEventListener('click', () => {
+    if(!mm_state) {
+        bt_mm.classList.add('open');
+        mm.classList.add('open');
+        mm_state = true;
+    } else {
+        bt_mm.classList.remove('open');
+        mm.classList.remove('open');
+        mm_state = false;
+    }
+});
