f40e497e87833720b9422e69b3f1fc2857c21ede
gustavosk01 <gustavo.sk01@hotmail.com>
2021-11-01 00:46:27 -0300

    V1.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,28 @@
-var array_produtos = [['miami vice memories', 'a1.jpg', 'oooopaa', '1.00', true], ['lembranças da sua mãe', '2b.jpg', 'dasdawdawdawd', '3.00', false], ['64gb', 'robbie.jpg', 'ggggggggggggggggg', '0.0002', false]];
+var array_produtos = [['miami vice memories', 'a1.jpg', 'oooopaa', '1.00', true], ['lembranças da sua mãe', '2b.jpg', 'dasdawdawdawd', '3.00', false], ['64gb', 'robbie.jpg', 'ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg', '0.0002', false]];
 
 
 window.onload = function() {
     criaProdutos();
 }
 
-
 function criaProdutos() {
     for(var i = 0; i < array_produtos.length; i++) {
         var conteudo = "";
-
-        
-
-
-
-
-        conteudo += ' <div class="card">';
-        conteudo += ' <div class="top-card">';
+        conteudo += '<div class="products-card">';
         conteudo += '<img src="media/' + array_produtos[i][1] + '">';
-        conteudo += ' </div>';
+        conteudo += '<h3>' + array_produtos[i][0] + '</h3>';
+        conteudo += '<p>' + array_produtos[i][2] + '</p>';
 
-        conteudo += ' <div class="middle-card">';
 
-        conteudo += '<h2 class="nomeDoProduto">' + array_produtos[i][0] + '</h2>';
-        conteudo += '<p class="descricao">' + array_produtos[i][2] + '</p>';
 
-        conteudo += ' </div>';
-
-        conteudo += ' <div class="bottom-card">';
-        conteudo += ' <b class="valor">BTC ' + array_produtos[i][3] + '</b>';
+        conteudo += ' <b>BTC ' + array_produtos[i][3] + '</b>';
 
         if (array_produtos[i][4] == false) {
-            conteudo += '<button class="comprar">Adicionar ao carrinho</button>';
+            conteudo += '<button class="products-card-comprar">Adicionar ao carrinho</button>';
         } else {
-            conteudo += '<button class="comprado">No carrinho</button>';
+            conteudo += '<button class="products-card-comprado">No carrinho</button>';
         }
-
-        conteudo += ' </div>';
-
-        conteudo += ' </div>';
-        conteudo += ' </div>';
-        
-        document.getElementById("produtos").innerHTML += conteudo;
-
-      
+        conteudo += '</div>';
+        document.getElementById("products-container").innerHTML += conteudo;
     }
 }
